c625da8ae4c34741317039a0e4f9984770881ca0
Charles Robinson <charlesdalerobinson@gmail.com>
2021-02-27 12:25:11 -0800

    Updated server.js to work with MongoDB.

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require("express");
+
+const mongoose = require("mongoose");
+
+const routes = require("./routes");
+
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Add routes, both API and view
+app.use(routes);
+
+// The code below was provided - I used the above line to get all the routes instead. 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/requestapp");
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
