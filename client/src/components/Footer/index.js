7838b13c61d518292ebd75b2993aaa0a90c34107
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2021-03-06 12:20:48 -0800

    Fix parsing error.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,24 +4,6 @@ import './style.css'
 function Footer(props) {
     return (
         <div>
-<<<<<<< Updated upstream
-            <nav className="navbar fixed-bottom navbar-light bg-dark mb-0">
-                <a className={props.current === "home" ? "navbar-brand col text-center current-link": "navbar-brand col text-center not-current"} href="/dj/dashboard">
-                    <i className="fas fa-home fa-2x"></i>
-                    <h3>Home</h3>
-                </a>
-                <a className={props.current === "request" ? "navbar-brand col text-center current-link": "navbar-brand col text-center not-current"} href="/dj/requests">
-                    <i className="fas fa-music fa-2x"></i>
-                    <h3>Requests</h3>
-                </a>
-                <a className={props.current === "activity" ? "navbar-brand col text-center current-link": "navbar-brand col text-center not-current"} href="/dj/activity">
-                    <i className="fas fa-sliders-h fa-2x"></i>
-                    <h3>Activity</h3>
-                </a>
-                <a className={props.current === "profile" ? "navbar-brand col text-center current-link": "navbar-brand col text-center not-current"} href="/dj/profile">
-                    <i className="fas fa-user fa-2x"></i>
-                    <h3>Profile</h3>
-=======
             <nav className="navbar fixed-bottom navbar-light bg-dark pb-3">
                 <a className="navbar-brand col text-center" href="#">
                     <i className={props.current === "home" ? "fas fa-home fa-2x current-link": "fas fa-home fa-2x"}></i>
@@ -38,7 +20,6 @@ function Footer(props) {
                 <a className="navbar-brand col text-center" href="#">
                     <i className={props.current === "profile" ? "fas fa-user fa-2x current-link": "fas fa-user fa-2x"}></i>
                     <h6 className={props.current === "profile" ? "current-link" : ""}>Profile</h6>
->>>>>>> Stashed changes
                 </a>    
             </nav>
         </div>
