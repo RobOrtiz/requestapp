06d6d0f75858a8a84ed538fe079c99f3d37f731b
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2021-03-11 13:45:42 -0800

    Update header style.  Add color to activity header table.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -7,11 +7,9 @@ function Header(props) {
     return (
         <div>
             <nav className="navbar fixed-top">
-                <a className="navbar-brand" href="/dj/dashboard">
-                <img src="https://i.ibb.co/bdJNc5g/Noi-Logo-200x200.png" alt="Noi-Logo-200x200" border="0" width="60" height="60" className="d-inline-block align-top" />
-                </a>
+                <img src="https://i.ibb.co/bdJNc5g/Noi-Logo-200x200.png" alt="Noi-Logo-200x200" border="0" width="95" height="95" className="navbar-brand d-inline-block align-top" />
                 {/* <LoginButton/><LogoutButton/> */}
-                <h1 className="text-center mr-2">{props.title}</h1>
+                <h1 className="text-center mr-3">{props.title}</h1>
             </nav>
         </div>
     )
