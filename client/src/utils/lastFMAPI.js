085ad4624da39e9bfe091f34b1b19326a8ef2019
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2021-03-14 17:33:23 -0700

    Add last.fm api for album covers.

--- client/src/utils/lastFMAPI.js
+++ client/src/utils/lastFMAPI.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-function lastFMAPI(artist, track) {
+export default {
+    findAlbumCover: function(track, artist) {
     const API_KEY = process.env.REACT_APP_LASTFM_API_KEY;
     const urlArtist = artist.trim().split(' ').join("+").toLowerCase();
     const urlTrack = track.trim().split(' ').join("+").toLowerCase();
 
-    let url = `http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${API_KEY}&artist=${urlArtist}=${urlTrack}&format=json`;
+    let url = `http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${API_KEY}&artist=${urlArtist}&track=${urlTrack}&format=json`;
 
     return axios.get(url);
+    }
 }
-
-export default lastFMAPI;
