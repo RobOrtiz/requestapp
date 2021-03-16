3d42dbe13b922d7e4f91f59994824be76db7e7ac
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2021-03-16 12:39:07 -0700

    Add react-to-print npm for printer-friendly qr code page.

--- client/src/components/QRModal/index.js
+++ client/src/components/QRModal/index.js
@@ -1,31 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
 import QR from "../QRCode";
 import { Row, Col } from "../Grid";
 
 
 function QRModal(props) {
+    const componentRef = useRef();
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+      });
 
     return(
-        <div className="modal fade mx-auto" id="qr-code-modal" tabindex="-1">
+        <div className="modal fade" id="qr-code-modal" tabindex="-1">
             <div className="modal-dialog">
                 <div className="modal-content">
-                {/* <div className="modal-header">
-                    <h5 className="modal-title">{props.djName}</h5>
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div> */}
-                <div className="modal-body">
+                <div className="modal-body" ref={componentRef}>
                     <div className="conatiner-fluid">
                         <Row classes="justify-content-center mt-5">
                             <img src="https://i.ibb.co/bdJNc5g/Noi-Logo-200x200.png" alt="Noi-Logo-200x200" border="0" width="150" height="150" />
                         </Row>
                         <Row classes="justify-content-center">
                             <Col size="12">
-                                <h1 className="qr-modal-name mt-5">{props.djName}</h1>
+                                <h1 className="qr-modal-name mt-5 text-center">{props.djName}</h1>
                             </Col>
-                            <h3 className="mt-3">LINK:</h3>
-                            <h3 className="qr-modal-link p-3"><a href={`http://localhost:3000/request/${props.djCode}`}>{`http://localhost:3000/request/${props.djCode}`}</a></h3>
+                                <h3 className="mt-3">LINK:</h3>
+                                <h3 className="qr-modal-link p-3"><a href={`http://localhost:3000/request/${props.djCode}`}>{`http://localhost:3000/request/${props.djCode}`}</a></h3>
                         </Row>
                         <Row classes="d-flex align-items-center justify-content-center qr-code">
                             <QR djCode={props.djCode} />                            
@@ -33,7 +32,8 @@ function QRModal(props) {
                     </div>
                 </div>
                 <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button className="btn btn-secondary rounded-circle" onClick={handlePrint} style={{fontSize: "1.5rem"}}>&#x1f5b6;</button>
+                    <button type="button" className="btn btn-secondary rounded-circle" data-dismiss="modal">&#x2716;</button>
                 </div>
                 </div>
             </div>
