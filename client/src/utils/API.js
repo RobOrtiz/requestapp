aa33da6132da1cd34742cc63ee3e4bf816381bb8
Brian Parker <btparker70@gmail.com>
2021-03-16 22:23:50 -0700

    event/dj info display request page. some functionality for checking if active event

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,6 +10,7 @@ export default {
   createRequest: function (requestData) {
     return axios.put("/api/djs/event", requestData)
   },
+
   // This is for checking if a logged in dj has a profile created.
   getDj: function (data) {
     return axios.get("/api/djs/?userSub=" + data)
@@ -29,27 +30,8 @@ export default {
     return axios.put("/api/djs/requests", songData)
   },
 
-  // getDj: function(data) {
-  //   return axios.get("/api/djs", data)
-  // },
   // Cloudinary API to upload image to their server and return a URL linking to the image for reference in Dj Profile and Dj Event.
   uploadImage: function (imageData) {
     return axios.post("https://api.cloudinary.com/v1_1/noimgmt/image/upload", imageData)
   }
-  // Gets all books
-  // getBooks: function() {
-  //   return axios.get("/api/books");
-  // },
-  // // Gets the book with the given id
-  // getBook: function(id) {
-  //   return axios.get("/api/books/" + id);
-  // },
-  // // Deletes the book with the given id
-  // deleteBook: function(id) {
-  //   return axios.delete("/api/books/" + id);
-  // },
-  // // Saves a book to the database
-  // saveBook: function(bookData) {
-  //   return axios.post("/api/books", bookData);
-
 };
