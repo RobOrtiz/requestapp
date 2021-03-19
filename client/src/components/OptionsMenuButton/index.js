d5fb6e8eeb29cc0c01a94063b8c0bd9738f1830d
Brian Parker <btparker70@gmail.com>
2021-03-18 19:24:45 -0700

    added image

---
+++ client/src/components/OptionsMenuButton/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function OptionsMenuButton() {
+
+  return (
+    <button type="button" class="btn btn-sm btn-dark mt-2" data-toggle="modal" data-target="#options-menu-modal">
+        OPTIONS
+    </button>
+  )
+}
+
+export default OptionsMenuButton;
