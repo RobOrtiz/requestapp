aa00b64d2993e42540fc47125113ddd3c2290172
btparker70 <33881192+btparker70@users.noreply.github.com>
2021-03-19 18:47:42 -0700

    Update index.js

--- client/src/components/OptionsMenuButton/index.js
+++ client/src/components/OptionsMenuButton/index.js
@@ -5,7 +5,6 @@ function OptionsMenuButton() {
   return (
     <button type="button" class="btn btn-sm btn-dark mt-2" data-toggle="modal" data-target="#options-menu-modal">
         OPTIONS 
-        {/* <i class="fas fa-cog"></i> */}
     </button>
   )
 }
